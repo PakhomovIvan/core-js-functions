e113f4aae853d3937fb3dfbdd6101dabf12221c2
Ivan Pakhomov <ip1133@ya.ru>
2024-04-15 20:24:47 +0300

    feat: add 5 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -108,8 +108,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(functions) {
+  const result = functions();
+  return () => result;
 }
 
 /**
